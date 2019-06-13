38e0767a86db13e152333eed0852a8076eede819
Leonardo Martinez <leos32111@gmail.com>
2019-06-13 02:37:59 -0400

    Camera and Movement foundation
    Initial foundation for the game, a basic first person camera with functional movement and modifiable jumping via the Unity interface.

---
+++ Assets/Scripts/Movement/Camera_Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    public enum RotationAxis
+    {
+        MouseX = 1, MouseY = 2
+    }
+
+    public RotationAxis axes = RotationAxis.MouseX;
+
+    // Minimum and Maximum vertical rotation as to stop infinite vertical rotation of the camera.
+    public float minVert = -45.0f;
+    public float maxVert = 45.0f;
+
+    // Horizontal and Vertical sensitivity.
+    public float sensHorz = 10.0f;
+    public float sensVert = 10.0f;
+
+    // Vertical angle at start.
+    public float rotationX = 0;
+
+    void Update()
+    {
+        if (axes == RotationAxis.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensHorz, 0);
+        }
+        else if (axes == RotationAxis.MouseY)
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * sensVert;
+
+            //Clamps vertical rotation
+            rotationX = Mathf.Clamp(rotationX, minVert, maxVert);
+        }
+
+        float rotationY = transform.localEulerAngles.y;
+
+        transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+    }
+}
---
+++ Assets/Scripts/Movement/Player_Movement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+    public float jumpForce = 7f;
+
+    public Rigidbody rb;
+    public LayerMask groundLayers;
+
+    private CharacterController charCont;
+
+    private bool isJumping = false;
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charCont = GetComponent<CharacterController>();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 move = new Vector3(deltaX, 0, deltaZ);
+        move = Vector3.ClampMagnitude(move, speed); // Speed Limit
+
+        move.y = gravity * 0.5f;
+
+        move *= Time.deltaTime;     // Speed does not change depending on framerate
+        move = transform.TransformDirection(move);
+        charCont.Move(move);
+
+        
+        // Jumping
+        if(!isJumping && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+       
+    }
+    
+    private IEnumerator JumpEvent()
+    {
+        charCont.slopeLimit = 90.0f;
+
+        float airTime = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(airTime);
+            charCont.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            airTime += Time.deltaTime;
+            yield return null;
+
+        } while (!charCont.isGrounded && charCont.collisionFlags != CollisionFlags.Above);
+
+        charCont.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+}
