49d2039a0adcd767130fdd5b5bf717e8a8fb075c
Leonardo Martinez <leos32111@gmail.com>
2019-06-21 13:35:50 -0400

    Updated Movement and Shooting, using Unity Asset Store Assets
    Title

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -8,22 +8,59 @@ public class Gun : MonoBehaviour
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
+    public AudioSource shotSound;
+
+    [System.Serializable]
+        public enum WeaponType
+        {
+            Auto = 0,
+            Semi = 1
+        }
+
+    public WeaponType type;
+
+    // Cooldown time between semi automatic shots
+    private float cooldown;
+    public float semiShootCooldown;
+
+    void Start()
+    {
+        cooldown = 0;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+            Debug.Log(cooldown + " ---- cooldown time");
 
-        if (Input.GetButtonDown("Fire1"))
+            // Safety in case computer's internal clock fucks up
+            if (cooldown < 0)
+            {
+                cooldown = 0;
+            }
+        }
+
+        if (Input.GetButtonDown("Fire1") && type == WeaponType.Semi && cooldown == 0)
         {
             Shoot();
+
+            cooldown = semiShootCooldown;
         }
+
+        
     }
 
+    // Raycast shooting system, Projectile weapons will have a seperate script entirely
     void Shoot()
     {
         RaycastHit hit;
 
         muzzleFlash.Play();
+
+        shotSound.Play();
 
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
@@ -35,8 +72,11 @@ public class Gun : MonoBehaviour
                 target.TakeDamage(damage);
             }
 
+            // Impact effect upon hitting a surface, readd when Particle System is created for it
+            /*
             GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impactGO, 2f);
+            */
         }
     }
 }
